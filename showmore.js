49de7c4cd41703f6690491327c43933503ccc8af
Larss123 <lars123stout@gmail.com>
2021-11-23 20:11:05 +0000

    Java geadd voor Info

--- showmore.js
+++ showmore.js
@@ -1,4 +1,18 @@
-ShowMore()
+function ShowMore()
 {
+    document.getElementById("info").classList.remove('showless');
+    document.getElementById("button").classList.remove('less');
     document.getElementById("info").classList.add('showmore');
+    document.getElementById("button").classList.add('more');
+    document.getElementById("dots").classList.add('nodots');
+}
+
+function ShowLess()
+{
+    document.getElementById("dots").classList.remove('nodots');
+    document.getElementById("info").classList.remove('showmore');
+    document.getElementById("button").classList.remove('more');
+    document.getElementById("info").classList.add('showless');
+    document.getElementById("button").classList.add('less');
+    
 }
